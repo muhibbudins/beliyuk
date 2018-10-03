ad6f37324ae4e19ae7174d44732bfd93454600db
Muhibbudin Suretno <muhibbudinsuretno1@gmail.com>
2018-10-04 05:33:45 +0700

    Add chokidar watcher

--- src/start.js
+++ src/start.js
@@ -12,10 +12,16 @@ module.exports = (halsa, project, config) => {
   const app = express()
   const target = path.join(project, '.halsa')
   const port = config['port'] || 3000
+  const reloadable = [
+    path.join(project, '/pages/'),
+    path.join(project, '/layouts/'),
+    path.join(project, '/themes/'),
+    path.join(project, '/static/')
+  ]
 
-  // if (!pathExistsSync(target)) {
+  if (!pathExistsSync(target)) {
     build(project)
-  // }
+  }
 
   app.set('port', port)
   app.get('/', (req, res) => {
@@ -24,7 +30,36 @@ module.exports = (halsa, project, config) => {
 
   app.use('/', express.static(target))
 
+  let first = true
+  const watcher = chokidar.watch(reloadable, {
+    ignored: /^\./,
+    persistent: true
+  })
   const server = http.createServer(app)
+  const reloadServer = reload(app)
+  const restart = (file) => {
+    if (first) {
+      console.log(`Watching file ${file.replace(project, '')} changed`)
+    } else {
+      console.log(`File ${file.replace(project, '')} changed`)
+    }
+
+    build(project)
+
+    reloadServer.reload()
+  }
+
+  setTimeout(() => {
+    first = !first
+  }, 2000)
+
+  watcher
+    .on('add', (path) => {restart(path)})
+    .on('change', (path) => restart(path))
+    .on('unlink', (path) => restart(path))
+    .on('error', (path) => {
+      process.exit(0)
+    })
 
   server.listen(app.get('port'), function () {
     console.log('Web server listening on port ' + app.get('port'))
